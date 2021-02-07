522a6a1ef1645db2595b7b0305db0d0eea44bdd4
Mykaelos <mmaderii@gmail.com>
2021-02-06 23:29:05 -0600

    Added a simple GizmosM.DrawPoint() method to avoid using DrawLine to make tiny vertical points. I should probably promote this class to MUL soon.

--- Generator/Utilities/GizmosM.cs
+++ Generator/Utilities/GizmosM.cs
@@ -11,5 +11,10 @@ namespace MykaelosUnityLevelLayoutGenerator.Utilities {
             Gizmos.color = color;
             Gizmos.DrawLine(vector1, vector2);
         }
+
+        public static void DrawPoint(Vector2 vector2, Color color, float size = 0.1f) {
+            Gizmos.color = color;
+            Gizmos.DrawWireCube(vector2, Vector3.one * size);
+        }
     }
 }
